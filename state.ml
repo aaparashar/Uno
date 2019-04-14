e21a8c696777e9ad1a35b0bde282e327fff324dc
Aarushi Parashar <ap635@cornell.edu>
2019-04-14 17:53:48 -0400

    Ran Make Clean Finish State Tests

--- state.ml
+++ state.ml
@@ -24,10 +24,19 @@ let get_players_hand st = st.players_hand
 let get_ai_hand st = st.ai_hand
 let get_draw_deck st = st.draw_deck
 
-let put c (st:t)  = if (is_valid c st.current_card) 
+let put c (st:t) s = if (is_valid c st.current_card && s="player") 
   then {current_card = c;
         players_hand = remove_card c st.players_hand; 
         ai_hand= st.ai_hand; 
         draw_deck=st.draw_deck; 
         playing_deck= add_card c st.playing_deck} 
+  else if (is_valid c st.current_card && s="ai")
+  then {current_card = c;
+        players_hand = st.players_hand
+            ai_hand= remove_card c st.ai_hand; 
+        draw_deck=st.draw_deck; 
+        playing_deck= add_card c st.playing_deck} 
   else raise Invalid_Move
+
+
+
