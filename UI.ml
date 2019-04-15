a19f28042dbfc4d482008b8a581b1010273449db
Aarushi Parashar <ap635@cornell.edu>
2019-04-15 19:56:21 -0400

    Finished AI State needs to be tested

--- UI.ml
+++ UI.ml
@@ -1,64 +1,66 @@
-Open Deck
-Open state
-Open ANSITerminal
-Open Format
+open Deck
+open state
+open ANSITerminal
+open Format
 
 let pp_card (c:card) = 
   ANSITerminal.(string_of_int[c.color] c.number)
 
 let rec print_hand d =
   match d with 
-    match d with 
-  match d with 
   | h::t -> "\t"^pp_card^"\n" ^ print_hand t
   | [] -> "\n"
 let print_card c =
   ANSITerminal.([c.color] (c.color+ " " c.number))
-  
+
 let rec do_play_game st =
-    if State.has_won st then
-        ANSITeriminal.(print_string [cyan]("\n You "));
-        if st.players_hand = 0 then ANSITeriminal.(print_string [cyan]("Win :D Thanks for playing!"));
-        else ANSITeriminal.(print_string [cyan]("Lose :( Better luck next time"));
-    else 
-        ANSITeriminal.(print_string [cyan]("\nLast Card on the Discard Pile:\t"));
-        print_string (print_card(st.current_card));
-        ANSITeriminal.(print_string [cyan]("\n In Your Hand:\n"));
-        print_string(print_hand(st.players_hand));
-        ANSITeriminal.(print_string [cyan] ("\nWhat's your next move?\n"));
-        print_string  "> ";
-    let (out_string,cmd) = 
+  if State.has_won st then
+    ANSITeriminal.(print_string [cyan]("\n You "));
+  if st.players_hand = 0 then ANSITeriminal.(print_string [cyan]("Win :D Thanks for playing!"));
+else ANSITeriminal.(print_string [cyan]("Lose :( Better luck next time"));
+else if st.turn
+    ANSITeriminal.(print_string [cyan]("\nLast Card on the Discard Pile:\t"));
+  print_string (print_card(st.current_card));
+  ANSITeriminal.(print_string [cyan]("\n In Your Hand:\n"));
+  print_string(print_hand(st.players_hand));
+  ANSITeriminal.(print_string [cyan] ("\nWhat's your next move?\n"));
+  print_string  "> ";
+  let (out_string,cmd) = 
     match Command.parse (read_line()) with
-      | exception (Command.Empty) -> ("\nWhat's your next move?\n","");
-      | exception (Command.Malformed) -> ("\n\nCommand not recognized\n","");
-      | Quit -> ("\n\nSee ya next time!\n")
-                  exit 0;
-      | Score -> ("Score:\t"^(string_of_int(State.get_current_score st))^"\n", "")
-      | Hand -> ("\n In Your Hand:\n"^print_hand(st.players_hand),"")
-      | Put(t)-> ("\n You played:\t"^print_card(t),"put")
-    in
-    match cmd with
-      | "put" -> 
-          (match State.put t st "player" with
-            | Invalid_Move -> ANSITeriminal(print_string[magenta]"\n Can't play that card \n");
-               do_play_game st
-            | Valid_Move(newState) -> ANSITeriminal(print_string[cyan] out_string); 
-                do_play_game newState) 
-      | _ -> ANSITeriminal(print_stringpcyan out_string);
-        do_play_game st
+    | exception (Command.Empty) -> ("\nWhat's your next move?\n","");
+    | exception (Command.Malformed) -> ("\n\nCommand not recognized\n","");
+    | Quit -> ("\n\nSee ya next time!\n")
+                exit 0;
+    | Score -> ("Score:\t"^(string_of_int(State.get_current_score st))^"\n", "")
+    | Hand -> ("\n In Your Hand:\n"^print_hand(st.players_hand),"")
+    | Put(t)-> ("\n You played:\t"^print_card(t),"put")
+    | Draw -> ("\n You drew a card.")
+  in
+  match cmd with
+  | "put" -> 
+    (match State.put t st "player" with
+     | Invalid_Move -> ANSITeriminal(print_string[magenta]"\n Can't play that card \n");
+       do_play_game st
+     | Valid_Move(newState) -> ANSITeriminal(print_string[cyan] out_string); 
+       do_play_game newState) 
+  | "draw"
+  | _ -> ANSITeriminal(print_stringpcyan out_string);
+    do_play_game st
+
+else st.ai_turn
 
 let play_game p = 
   ANSITerminal.(print_string [cyan] "Welcome, "^pname^" to the Uno Casino.");
   ANSITerminal.(print_string [cyan] "\nLet's deal you in");  
   do_play_game (State.init_state)
 
-  
+
 
 let rec main () = 
   ANSITerminal.(print_string [cyan]
                   "\n\nWelcome to the Uno Game Engine\n"); 
-     print_endline "Please enter your player name:\n";
-     print_string  "> ";
-     match read_line () with
-     | "" -> main ()
-     | pname -> play_game pname 
+  print_endline "Please enter your player name:\n";
+  print_string  "> ";
+  match read_line () with
+  | "" -> main ()
+  | pname -> play_game pname 
--- state.ml
+++ state.ml
@@ -5,6 +5,7 @@ type t = {
   ai_hand : Deck.t;
   draw_deck : Deck.t ;
   playing_deck : Deck.t ;
+  turn:bool;
 } 
 exception Invalid_Move
 let init_state  = 
@@ -15,7 +16,7 @@ let init_state  =
   let playing = add_card (top_card remaining) empty_deck in
   let remaining2 = remove_card (top_card remaining) remaining in
   {current_card = top_card playing; players_hand = players; ai_hand= ai_deck; 
-   draw_deck=remaining2; playing_deck= playing}
+   draw_deck=remaining2; playing_deck= playing; turn = true}
 
 let get_current_card (st:t) = st.current_card
 let get_players_hand st = st.players_hand
@@ -23,20 +24,46 @@ let get_ai_hand st = st.ai_hand
 let get_draw_deck st = st.draw_deck
 let has_won st = Deck.len st.players_hand = 0 || Deck.len st.ai_hand = 0
 let get_current_score = failwith "Unimplemented"
+let get_turn st = st.turn
 
-let put c (st:t) s = if (is_valid c st.current_card && s="player") 
+
+let put c (st:t) s = if (is_valid c st.current_card && s="player" && contains c st.player_hand) 
   then Valid_Move {current_card = c;
-        players_hand = remove_card c st.players_hand; 
-        ai_hand= st.ai_hand; 
-        draw_deck=st.draw_deck; 
-        playing_deck= add_card c st.playing_deck} 
-  else if (is_valid c st.current_card && s="ai")
+                   players_hand = remove_card c st.players_hand; 
+                   ai_hand= st.ai_hand; 
+                   draw_deck=st.draw_deck; 
+                   playing_deck= add_card c st.playing_deck; turn = false;} 
+  else if (is_valid c st.current_card && s="ai" && contains c st.ai_hand )
   then {current_card = c;
         players_hand = st.players_hand;
-            ai_hand= remove_card c st.ai_hand; 
+        ai_hand= remove_card c st.ai_hand; 
         draw_deck=st.draw_deck; 
-        playing_deck= add_card c st.playing_deck} 
+        playing_deck= add_card c st.playing_deck; turn = true;} 
   else raise Invalid_Move
 
+
+
+let draw (st:t) s = 
+  if (draw_deck = empty_deck && s= "player") then 
+    let reset = {st with draw_deck=shuffle (remove_card(st.current_card st.playing_deck)); 
+                         playing_deck = add_card(st.current_card empty_deck)} in 
+    {reset with players_hand = (add_card (top_card reset.draw_deck) reset.players_hand); 
+                draw_deck = remove_card (top_card reset.draw_deck) reset.draw_deck; turn = false;}
+  else if (draw_deck = empty_deck && s= "ai") then 
+    let reset = {st with draw_deck=shuffle (remove_card(st.current_card st.playing_deck)); 
+                         playing_deck = add_card(st.current_card empty_deck)} in 
+    {reset with ai_hand = (add_card (top_card reset.draw_deck) reset.ai_hand); 
+                draw_deck = remove_card (top_card reset.draw_deck) reset.draw_deck;turn=true;}
+  else if (s = "ai") then 
+    {st with ai_hand = (add_card (top_card st.draw_deck) st.ai_hand); 
+             draw_deck = remove_card (top_card st.draw_deck) st.draw_deck;turn=true;}
+  else 
+    {st with player_hand = (add_card (top_card st.draw_deck) st.player_hand); 
+             draw_deck = remove_card (top_card st.draw_deck) st.draw_deck;turn=false;}
+
+let ai_turn st = 
+  match (get_valid_card st.current_card st.ai_hand) with
+  |None -> draw st "ai"
+  |Some x -> put x st "ai"
 
 
